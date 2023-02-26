4a9e8d75fd0c08c869c4beb70e477fbdd75beda9
musleh0001 <khan.muslehuddin0001@gmail.com>
2023-02-27 00:24:53 +0600

    feat: item and category list view

--- core/views.py
+++ core/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+from item import models
+
+
 def index(request):
-    return render(request, "core/index.html")
+    items = models.Item.objects.filter(is_sold=False)[0:6]
+    categories = models.Category.objects.all()
+
+    return render(
+        request, "core/index.html", {"categories": categories, "items": items}
+    )
+
 
 def contact(request):
     return render(request, "core/contact.html")
